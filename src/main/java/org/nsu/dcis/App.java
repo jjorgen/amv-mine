9204f49d7b5aaccccec013a38e570d8c9183f82c
amv <jjorgensen2311@gmail.com>
2017-10-21 20:27:05 -0400

    First commit

---
+++ src/main/java/org/nsu/dcis/App.java
@@ -0,0 +1,13 @@
+package org.nsu.dcis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
