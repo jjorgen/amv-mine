66b9de88b887dfa643a47a7ddd93bf6093c328b8
jorgej2 <jorgej2@nationwide.com>
2018-01-28 20:31:50 -0500

    Adding more test methods for aspect mining for cross cutting concerns that implements interfaces.

--- amv-controller/src/test/java/org/nsu/dcis/amv/core/service/CrossCuttingConcernAsInterfaceTest.java
+++ amv-controller/src/test/java/org/nsu/dcis/amv/core/service/CrossCuttingConcernAsInterfaceTest.java
@@ -40,6 +40,100 @@ public class CrossCuttingConcernAsInterfaceTest {
     private static final String ROOT_DIRECTORY = "C:/WS/amvCore/src/test/java/org/nsu/dcis/amv/core/support";
     private Logger log = Logger.getLogger(getClass().getName());
 
+    /**
+     * Developer Note:
+     *   Conclusions and further work. The code in this test suite demonstrates:
+     *   (1) How it is possible to get interfaces for a class which that contains a method.
+     *   (2) That it is possible to get interface that the current interface is extending.
+     *   (3) That it is possible to get the methods that an interface implements.
+     *   (4) That insufficient information is enclosed in the AST so tht it is not possible
+     *       to get the interface that an interface extends and then get the method names from
+     *       the extended interface directly. This because the full path of the extended interface
+     *       is not available.
+     *   (5) In order to work around this the following can be done when building the AST:
+     *       (5.1) Create a separate collection of all the CompilationUnitWrapper classes which
+     *             are instantiated from interfaces.
+     *       (5.2) Search this collection by interface name when trying to find the methods that
+     *             is present in an ancestor interface. The CompilationUnitWrapper will have this
+     *             information since it was created from the full path of the interface.
+     *       (5.3) Also, (5.2) is best implemented as a recursive method since many interfaces may
+     *             be present, each with their own inheritance hierarchy.
+     *
+     * @throws Exception
+     */
+
+
+
+    // Developer Note:
+    // The following algorithm will be used when looking for implementing interface for a method.
+    //
+    //    Algorithm that will be used to locate interface that implement method(s)
+    //            --------------------------------------------------------------------------------------------------------
+    //            for each interface look for match of implementing method in the interface
+    //
+    //	1. after a particular interface has been searched the look for ancester interface if this exists
+    //	2. search the ancester interface for the method
+    //	3. repeat (2) as long as there are ancester interfaces to search
+    //
+    //    stop when an interface that implements the method has been found
+    //	if the interface can not be instantiated with CompilationUnit as wiil be the case with EventListener then stop
+    //    Concusion: The result is inconclusive.
+    //
+    //    Also consider
+    //	-------------
+    //            if the methods are not the same then for the second method perform the same analysis
+    //	if an implementing interface is not found then stop -> not to be included in the set
+    //
+    //	if an implementing interface is found then
+    //            check if the two interfaces for the two methods are the same.
+
+
+    @Test
+    public void getImplementingInterfaceForMethodIncludingAncestorInterface() throws Exception {
+        String matchingMethodName = "getFont";
+        boolean methodWasFound = false;
+
+        CompilationUnitWrapper compilationUnitWrapper = new CompilationUnitWrapper("C:/work/0_NSU/CH/ifa/draw/contrib/html/AttributeContentProducerContext.java");
+        CompilationUnit compilationUnit = compilationUnitWrapper.getCompilationUnit("C:/work/0_NSU/CH/ifa/draw/contrib/html/AttributeContentProducerContext.java");
+
+        List<MethodRepresentation> methodRepresentations = compilationUnitWrapper.getMethodRepresentations();
+        for (MethodRepresentation methodRepresentation : methodRepresentations) {
+            log.info("Interface method name: " + methodRepresentation.getMethodName());
+            if (matchingMethodName.equals(methodRepresentation.getMethodName())) {
+                methodWasFound = true;
+                log.info("Method '" + methodRepresentation.getMethodName() + "' was found");
+                break;
+            }
+            if (!methodWasFound) {
+                log.info("Method '" + methodRepresentation.getMethodName() + "' was not found");
+            }
+        }
+
+        if (!methodWasFound) {
+            NodeList<TypeDeclaration<?>> types = compilationUnit.getTypes();
+            for (TypeDeclaration type : types) {
+                if (type instanceof ClassOrInterfaceDeclaration && ((ClassOrInterfaceDeclaration) type).isInterface()) {
+                    NodeList<ClassOrInterfaceType> extendingInterfaces = ((ClassOrInterfaceDeclaration) type).getExtends();
+                    for (ClassOrInterfaceType extendingInterface : extendingInterfaces) {
+                        System.out.println("The extending interface is: " + extendingInterface);
+
+//                        NodeList<TypeDeclaration<?>> types = extendingInterface.getTypes();
+//                        for (TypeDeclaration type : types) {
+//                            if (type instanceof ClassOrInterfaceDeclaration && !((ClassOrInterfaceDeclaration) type).isInterface()) {
+//                                methodImplements = ((ClassOrInterfaceDeclaration) type).getImplements();
+//                            }
+//                        }
+//
+//
+//
+//                        methodImplements = extendingInterface.getImplements();
+//
+                    }
+                }
+            }
+        }
+    }
+
     @Test
     public void getCrossCuttingConcernsAsInterfaceTest() throws Exception {
         CodeCloneMiningResult codeCloneMiningResult =
@@ -109,8 +203,8 @@ public class CrossCuttingConcernAsInterfaceTest {
         log.info("The Clone Count was: " + codeCloneMiningResult.getClone().size());
         log.info("**********************************************************************************");
         for (CodeCloneResult CodeCloneResult : codeClones) {
-            NodeList<ClassOrInterfaceType> firstMethodImplements = methodImplements(CodeCloneResult.getMethodPair().getFirst());
-            NodeList<ClassOrInterfaceType> secondMethodImplements = methodImplements(CodeCloneResult.getMethodPair().getSecond());
+            NodeList<ClassOrInterfaceType> firstMethodImplements = enclosingClassImplements(CodeCloneResult.getMethodPair().getFirst());
+            NodeList<ClassOrInterfaceType> secondMethodImplements = enclosingClassImplements(CodeCloneResult.getMethodPair().getSecond());
             Set<String> commonInterfaceNames = getInterfacesInCommon(firstMethodImplements, secondMethodImplements);
             if (commonInterfaceNames.size() > 0) {
                 ++codeCloneWithComminInterface;
@@ -132,8 +226,8 @@ public class CrossCuttingConcernAsInterfaceTest {
         log.info("The Before Advice Candidate Count was: " + codeCloneMiningResult.getBeforeAdviceCandidates().size());
         log.info("**********************************************************************************");
         for (CodeCloneResult codeCloneResult : beforeAdviceCandidates) {
-            NodeList<ClassOrInterfaceType> firstMethodImplements = methodImplements(codeCloneResult.getMethodPair().getFirst());
-            NodeList<ClassOrInterfaceType> secondMethodImplements = methodImplements(codeCloneResult.getMethodPair().getSecond());
+            NodeList<ClassOrInterfaceType> firstMethodImplements = enclosingClassImplements(codeCloneResult.getMethodPair().getFirst());
+            NodeList<ClassOrInterfaceType> secondMethodImplements = enclosingClassImplements(codeCloneResult.getMethodPair().getSecond());
             Set<String> commonInterfaceNames = getInterfacesInCommon(firstMethodImplements, secondMethodImplements);
             if (commonInterfaceNames.size() > 0) {
                 ++beforeAdviceCandidatesWithComminInterface;
@@ -155,8 +249,8 @@ public class CrossCuttingConcernAsInterfaceTest {
         log.info("The After Advice Candidate Count was: " + codeCloneMiningResult.getAfterAdviceCandidates().size());
         log.info("**********************************************************************************");
         for (CodeCloneResult codeCloneResult : afterAdviceCandidates) {
-            NodeList<ClassOrInterfaceType> firstMethodImplements = methodImplements(codeCloneResult.getMethodPair().getFirst());
-            NodeList<ClassOrInterfaceType> secondMethodImplements = methodImplements(codeCloneResult.getMethodPair().getSecond());
+            NodeList<ClassOrInterfaceType> firstMethodImplements = enclosingClassImplements(codeCloneResult.getMethodPair().getFirst());
+            NodeList<ClassOrInterfaceType> secondMethodImplements = enclosingClassImplements(codeCloneResult.getMethodPair().getSecond());
             Set<String> commonInterfaceNames = getInterfacesInCommon(firstMethodImplements, secondMethodImplements);
             if (commonInterfaceNames.size() > 0) {
                 ++afterAdviceCandidatesWithComminInterface;
@@ -179,8 +273,8 @@ public class CrossCuttingConcernAsInterfaceTest {
         log.info("**********************************************************************************");
         List<CodeCloneResult> aroundAdviceCandidates = codeCloneMiningResult.getAroundAdviceCandidates();
         for (CodeCloneResult codeCloneResult : aroundAdviceCandidates) {
-            NodeList<ClassOrInterfaceType> firstMethodImplements = methodImplements(codeCloneResult.getMethodPair().getFirst());
-            NodeList<ClassOrInterfaceType> secondMethodImplements = methodImplements(codeCloneResult.getMethodPair().getSecond());
+            NodeList<ClassOrInterfaceType> firstMethodImplements = enclosingClassImplements(codeCloneResult.getMethodPair().getFirst());
+            NodeList<ClassOrInterfaceType> secondMethodImplements = enclosingClassImplements(codeCloneResult.getMethodPair().getSecond());
             Set<String> commonInterfaceNames = getInterfacesInCommon(firstMethodImplements, secondMethodImplements);
             if (commonInterfaceNames.size() > 0) {
                 ++aroundAdviceCandidatesWithCommonInterface;
@@ -201,7 +295,7 @@ public class CrossCuttingConcernAsInterfaceTest {
         for (ClassOrInterfaceType firstInterfaceType : firstMethodImplements) {
             String firstInterfaceName = firstInterfaceType.getName();
             for (ClassOrInterfaceType secondInterfaceType : secondMethodImplements) {
-                String secondInterfaceName = firstInterfaceType.getName();
+                String secondInterfaceName = secondInterfaceType.getName();
                 if (secondInterfaceName.equals(firstInterfaceName)) {
                     commonInterfaceNames.add(firstInterfaceType.getName());
                 }
@@ -210,7 +304,7 @@ public class CrossCuttingConcernAsInterfaceTest {
         return commonInterfaceNames;
     }
 
-    private NodeList<ClassOrInterfaceType>  methodImplements(MethodRepresentation firstMethodRepresentation) {
+    private NodeList<ClassOrInterfaceType> enclosingClassImplements(MethodRepresentation firstMethodRepresentation) {
         NodeList<ClassOrInterfaceType> methodImplements = null;
         CompilationUnitWrapper compilationUnitWrapper = new CompilationUnitWrapper(firstMethodRepresentation.getFilePath());
         try {
